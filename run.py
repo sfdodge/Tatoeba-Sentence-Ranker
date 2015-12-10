7ff9629128ff00af13bd77917638773ca782a732
sfdodge <sfdodge>
2015-12-09 20:18:59 -0700

    now works fully but has problem with dupes

--- run.py
+++ run.py
@@ -1,31 +1,48 @@
 """
 Tatoeba sentence extractor
+
+Extracts sentences and orders them such that setences with more
+frequent words come first
+
+To do:
+1. Remove duplicates? (check for links in the same language
+2. implement qt interface
+3. create readme file
+4. Compile exe?
+5. pep check
+6. github
+7. add support for japanese/chinese
 """
 import urllib
 import tarfile
 import os
+from operator import itemgetter
+import csv
 
 # PARAMS
-MAX_SENTENCES = 10000
+MAX_SENTENCES = 5000000
+OUTPUT_NUMBER = 1000
+OUTFILE = 'output.csv'
 
 def main():
     # load data
-    sentences = loadData("spa","eng")
+    sentences = loadData("fra","eng")
 
     # compute word rank
     word_rank = computeFrequency(sentences)
 
     # score all sentences
-    sentence_score = computeScore(setences,word_rank)
+    sentence_score = computeScore(sentences,word_rank)
 
-    # now simply pop off as much as you want
-    f.open()
-    for i in range(N):
-        s = sentence_score.pop()
+    # now simply pop off sentences
+    with open(OUTFILE, 'w') as f:
+        writer=csv.writer(f)
 
-        writeToCSV(f,s)
+        for i in range(OUTPUT_NUMBER):
+            s_ind = sentence_score.pop(0)
+            s = sentences[s_ind[0]]
 
-    f.close()
+            writer.writerow(s)
 
 def loadData(lang1,lang2):
     """
@@ -70,20 +87,22 @@ def loadData(lang1,lang2):
     # now get the pairs in our languages
     # lang1 is first
     pairs = []
+    dupes = {}
     with open("links.csv") as ins:
         print "Computing pairs..."
         for line in ins:
             line = line[0:-1] # gets rid of newline
             split = line.split("\t")
             if split[0] in sentences and split[1] in sentences:
-                if sentences[split[0]][0] == sentences[split[1]][0]:
-                    continue # the languages are the same
-                    
                 if sentences[split[0]][0] == lang1:
-                    pairs.append((sentences[split[0]][1], sentences[split[1]][1]))
-                else:
-                    pairs.append((sentences[split[1]][1], sentences[split[0]][1]))
-                    
+                    # if sentences are the same, flag as a duplicate
+                    if sentences[split[0]][0] == sentences[split[1]][0]:
+                        dupes[max(split[0],split[0])] = 1
+                        continue 
+
+                    if split[0] not in dupes:
+                        pairs.append((sentences[split[0]][1], sentences[split[1]][1]))
+                        
     return pairs
 
 def computeFrequency(sentences):
@@ -99,14 +118,7 @@ def computeFrequency(sentences):
     word_count = {}
     for s in sentences:
         s = s[0]
-        # make lowercase
-        s = s.lower()
-
-        # strip punctuation
-        import string
-        s = s.translate(string.maketrans("",""), string.punctuation)
-
-        words = s.split(" ")
+        words = sentenceToWords(s)
         for w in words:
             try:
                 word_count[w] += 1
@@ -114,9 +126,8 @@ def computeFrequency(sentences):
                 word_count[w] = 1
 
     # now sort
-    import operator
     sorted_words = sorted(word_count.items(),
-                      key=operator.itemgetter(1),
+                      key=itemgetter(1),
                       reverse=True)
 
     # now output dictionary with key=word value=rank
@@ -125,5 +136,47 @@ def computeFrequency(sentences):
         word_rank[w[0]] = i + 1
 
     return word_rank
+
+def sentenceToWords(sentence):
+    """
+    Splits a sentence into a list of words
+    """
+    # make lowercase
+    s = sentence.lower()
+
+    # strip punctuation
+    import string
+    s = s.translate(string.maketrans("",""), string.punctuation)
+
+    words = s.split(" ")
+    return words
+
+def computeScore(sentences,word_rank):
+    """
+    Computes a score for each sentence. This is stored as a dictionary where the
+    key is the sentence id and the value is the score
+
+    The score is computing by summing the rank of the words. When this is sorted
+    then we will get sentences with smaller ammounts of high frequency words
+    """
+    print "Computing best sentences..."
+
+    scores = []
+    for i,s in enumerate(sentences):
+        words = sentenceToWords(s[0])
+
+        score = 0
+        for w in words:
+            try:
+                score += word_rank[w]
+            except:
+                print "Not in dictionary"
+                pass # wasn't able to find the word in the dictionary
+
+        scores.append((i,score))
+
+    # now sort by scores
+    scores.sort(key=itemgetter(1))
+    return scores
 
 main()
