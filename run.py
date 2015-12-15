c1cbecd0cbb924f95ce5efa96757eb545872a309
sfdodge <sfdodge>
2015-12-14 22:21:12 -0700

    started qt interface

--- run.py
+++ run.py
@@ -7,6 +7,7 @@ frequent words come first
 To do:
 1. Remove duplicates? (check for links in the same language
 2. implement qt interface
+2a. threading see -> https://wiki.python.org/moin/PyQt/Threading,_Signals_and_Slots
 3. create readme file
 4. Compile exe?
 5. pep check
@@ -18,165 +19,241 @@ import tarfile
 import os
 from operator import itemgetter
 import csv
+import sys
+import urllib2
+from PyQt4.QtCore import *
+from PyQt4.QtGui import *
 
 # PARAMS
 MAX_SENTENCES = 5000000
 OUTPUT_NUMBER = 1000
 OUTFILE = 'output.csv'
 
-def main():
-    # load data
-    sentences = loadData("fra","eng")
+class SentenceScorer(QObject):
+    def __init__(self,lang1,lang2):
+        super(SentenceScorer, self).__init__()
 
-    # compute word rank
-    word_rank = computeFrequency(sentences)
+        self.lang1 = lang1
+        self.lang2 = lang2
 
-    # score all sentences
-    sentence_score = computeScore(sentences,word_rank)
-
-    # now simply pop off sentences
-    with open(OUTFILE, 'w') as f:
-        writer=csv.writer(f)
-
-        for i in range(OUTPUT_NUMBER):
-            s_ind = sentence_score.pop(0)
-            s = sentences[s_ind[0]]
-
-            writer.writerow(s)
-
-def loadData(lang1,lang2):
-    """
-    Loads the sentences from tatoeba
-    """
-
-    # if data is not downloaded, the download
-    sentence_path = "http://downloads.tatoeba.org/exports/sentences.tar.bz2"
-    if not os.path.isfile("sentences.csv"):
-        sentence_file = urllib.URLopener()
-        print "Downloading sentences..."
-        sentence_file.retrieve(sentence_path, "sentences.tar.gz")
-        tar = tarfile.open("sentences.tar.gz")
-        tar.extractall()
-        tar.close()
-        os.remove("sentences.tar.gz")
-
-    link_path = "http://downloads.tatoeba.org/exports/links.tar.bz2"
-    if not os.path.isfile("links.csv"):
-        link_file = urllib.URLopener()
-        print "Downloading links..."
-        link_file.retrieve(link_path, "links.tar.gz")
-        tar = tarfile.open("links.tar.gz")
-        tar.extractall()
-        tar.close()
-        os.remove("links.tar.gz")
-
-    # now load all of the sentences in our target languages
-    sentences = {}
-    with open("sentences.csv", "r") as ins:
-        print "Getting sentences in chosen languages..."
-        for line in ins:
-            line = line[0:-1]   # gets rid of newline
-            split = line.split("\t")
-            if split[1] == lang1 or split[1] == lang2:
-                sentences[split[0]] = (split[1],split[2])
-
-            if len(sentences) > MAX_SENTENCES:
-                print "Reached sentence limit"
-                break
+    def sendMessage(self,msg):
+        """
+        Function to send progress messages
+        """
+        print msg
+        self.emit(SIGNAL("message"), msg)
         
-    # now get the pairs in our languages
-    # lang1 is first
-    pairs = []
-    dupes = {}
-    with open("links.csv") as ins:
-        print "Computing pairs..."
-        for line in ins:
-            line = line[0:-1] # gets rid of newline
-            split = line.split("\t")
-            if split[0] in sentences and split[1] in sentences:
-                if sentences[split[0]][0] == lang1:
-                    # if sentences are the same, flag as a duplicate
-                    if sentences[split[0]][0] == sentences[split[1]][0]:
-                        dupes[max(split[0],split[0])] = 1
-                        continue 
+    def generateScore(self):
+        """
+        Main function to generate everything
+        """
+        # load data
+        sentences = self.loadData()
 
-                    if split[0] not in dupes:
-                        pairs.append((sentences[split[0]][1], sentences[split[1]][1]))
-                        
-    return pairs
+        # compute word rank
+        word_rank = self.computeFrequency(sentences)
 
-def computeFrequency(sentences):
-    """
-    Computes the word frequency
+        # score all sentences
+        sentence_score = self.computeScore(sentences,word_rank)
 
-    Returns a large dictionary where the keys are the word, and the
-    value is the rank
-    """
-    print "Counting words..."
-    
-    # first count word frequencies
-    word_count = {}
-    for s in sentences:
-        s = s[0]
-        words = sentenceToWords(s)
-        for w in words:
-            try:
-                word_count[w] += 1
-            except:
-                word_count[w] = 1
+        # now simply pop off sentences
+        with open(OUTFILE, 'w') as f:
+            writer=csv.writer(f)
 
-    # now sort
-    sorted_words = sorted(word_count.items(),
-                      key=itemgetter(1),
-                      reverse=True)
+            for i in range(OUTPUT_NUMBER):
+                s_ind = sentence_score.pop(0)
+                s = sentences[s_ind[0]]
 
-    # now output dictionary with key=word value=rank
-    word_rank = {}
-    for i,w in enumerate(sorted_words):
-        word_rank[w[0]] = i + 1
+                writer.writerow(s)
 
-    return word_rank
+    sendMessage("Done...Output is in output.csv")
 
-def sentenceToWords(sentence):
-    """
-    Splits a sentence into a list of words
-    """
-    # make lowercase
-    s = sentence.lower()
+    def loadData(self):
+        """
+        Loads the sentences from tatoeba
+        """
 
-    # strip punctuation
-    import string
-    s = s.translate(string.maketrans("",""), string.punctuation)
+        # if data is not downloaded, the download
+        sentence_path = "http://downloads.tatoeba.org/exports/sentences.tar.bz2"
+        if not os.path.isfile("sentences.csv"):
+            sentence_file = urllib.URLopener()
+            self.sendMessage("Downloading sentences...")
+            sentence_file.retrieve(sentence_path, "sentences.tar.gz")
+            tar = tarfile.open("sentences.tar.gz")
+            tar.extractall()
+            tar.close()
+            os.remove("sentences.tar.gz")
 
-    words = s.split(" ")
-    return words
+        link_path = "http://downloads.tatoeba.org/exports/links.tar.bz2"
+        if not os.path.isfile("links.csv"):
+            link_file = urllib.URLopener()
+            self.sendMessage("Downloading links...")
+            link_file.retrieve(link_path, "links.tar.gz")
+            tar = tarfile.open("links.tar.gz")
+            tar.extractall()
+            tar.close()
+            os.remove("links.tar.gz")
 
-def computeScore(sentences,word_rank):
-    """
-    Computes a score for each sentence. This is stored as a dictionary where the
-    key is the sentence id and the value is the score
+        # now load all of the sentences in our target languages
+        sentences = {}
+        with open("sentences.csv", "r") as ins:
+            self.sendMessage("Getting sentences in chosen languages...")
+            for line in ins:
+                line = line[0:-1]   # gets rid of newline
+                split = line.split("\t")
+                if split[1] == self.lang1 or split[1] == self.lang2:
+                    sentences[split[0]] = (split[1],split[2])
 
-    The score is computing by summing the rank of the words. When this is sorted
-    then we will get sentences with smaller ammounts of high frequency words
-    """
-    print "Computing best sentences..."
+                if len(sentences) > MAX_SENTENCES:
+                    print "Reached sentence limit"
+                    break
 
-    scores = []
-    for i,s in enumerate(sentences):
-        words = sentenceToWords(s[0])
+        # now get the pairs in our languages
+        # lang1 is first
+        pairs = []
+        dupes = {}
+        with open("links.csv") as ins:
+            self.sendMessage("Computing pairs...")
+            for line in ins:
+                line = line[0:-1] # gets rid of newline
+                split = line.split("\t")
+                if split[0] in sentences and split[1] in sentences:
+                    if sentences[split[0]][0] == self.lang1:
+                        # if sentences are the same, flag as a duplicate
+                        if sentences[split[0]][0] == sentences[split[1]][0]:
+                            dupes[max(split[0],split[0])] = 1
+                            continue 
 
-        score = 0
-        for w in words:
-            try:
-                score += word_rank[w]
-            except:
-                print "Not in dictionary"
-                pass # wasn't able to find the word in the dictionary
+                        if split[0] not in dupes:
+                            pairs.append((sentences[split[0]][1], sentences[split[1]][1]))
 
-        scores.append((i,score))
+        return pairs
 
-    # now sort by scores
-    scores.sort(key=itemgetter(1))
-    return scores
+    def computeFrequency(self,sentences):
+        """
+        Computes the word frequency
 
-main()
+        Returns a large dictionary where the keys are the word, and the
+        value is the rank
+        """
+        self.sendMessage("Counting words...")
+
+        # first count word frequencies
+        word_count = {}
+        for s in sentences:
+            s = s[0]
+            words = self.sentenceToWords(s)
+            for w in words:
+                try:
+                    word_count[w] += 1
+                except:
+                    word_count[w] = 1
+
+        # now sort
+        sorted_words = sorted(word_count.items(),
+                        key=itemgetter(1),
+                        reverse=True)
+
+        # now output dictionary with key=word value=rank
+        word_rank = {}
+        for i,w in enumerate(sorted_words):
+            word_rank[w[0]] = i + 1
+
+        return word_rank
+
+    def sentenceToWords(self,sentence):
+        """
+        Splits a sentence into a list of words
+        """
+        # make lowercase
+        s = sentence.lower()
+
+        # strip punctuation
+        import string
+        s = s.translate(string.maketrans("",""), string.punctuation)
+
+        words = s.split(" ")
+        return words
+
+    def computeScore(self,sentences,word_rank):
+        """
+        Computes a score for each sentence. This is stored as a dictionary where the
+        key is the sentence id and the value is the score
+
+        The score is computing by summing the rank of the words. When this is sorted
+        then we will get sentences with smaller ammounts of high frequency words
+        """
+        self.sendMessage("Computing best sentences...")
+
+        scores = []
+        for i,s in enumerate(sentences):
+            words = self.sentenceToWords(s[0])
+
+            score = 0
+            for w in words:
+                try:
+                    score += word_rank[w]
+                except:
+                    print "Not in dictionary"
+                    pass # wasn't able to find the word in the dictionary
+
+            scores.append((i,score))
+
+        # now sort by scores
+        scores.sort(key=itemgetter(1))
+        return scores
+
+
+class Form(QDialog):
+    def __init__(self, parent=None):
+        super(Form, self).__init__(parent)
+
+        langs = ('English',
+                 'French',
+                 'Spanish',
+                 'Italian',
+                 'German',
+                 'Dutch',
+                 'Portugese',
+                 'Greek',
+                 'Russian',
+                 'Arabic',
+                 'Chinese',
+                 'Japanese',
+                 'Korean',
+                 'Thai')
+
+        self.fromComboBox = QComboBox()
+        self.fromComboBox.addItems(langs)
+        #self.fromComboBox.setValue('English')
+        self.fromSpinBox = QDoubleSpinBox()
+        self.fromSpinBox.setRange(1, 100000)
+        self.fromSpinBox.setValue(1000)
+        self.toComboBox = QComboBox()
+        self.toComboBox.addItems(langs)
+        #self.toComboBox.setValue('Spanish')
+        self.statusLabel = QLabel("")
+        self.generateButton = QPushButton('Generate...')
+        self.generateButton.clicked.connect(self.generate)
+
+        grid = QGridLayout()
+        grid.addWidget(self.fromComboBox, 0, 0)
+        grid.addWidget(self.fromSpinBox, 0, 1)
+        grid.addWidget(self.toComboBox, 1, 0)
+        grid.addWidget(self.generateButton,2,1)
+        grid.addWidget(self.statusLabel, 2, 0)
+        self.setLayout(grid)
+
+        self.setWindowTitle("Tatoeba Sentence Generator")
+
+    def generate(self):
+        self.generateButton.setDisabled(True)
+        s = SentenceScorer('fra','eng')
+        s.connect(s,SIGNAL('message'),self.statusLabel.setText)
+        s.generateScore()
+        self.generateButton.setEnabled(True)
+
+app = QApplication(sys.argv)
+form = Form()
+form.show()
+app.exec_()
